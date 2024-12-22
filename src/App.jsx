9d285b931b6ede35455ba5a4044135490ede79cb
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-12-22 00:23:48 -0300

    adiciona configuração inicial do projeto com React e Vite, incluindo estilos globais e componentes básicos

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { GlobalStyle } from './assets/styles/global';
+import { Container } from './App.styles';
+
+export default function App() {
+
+  return (
+    <Container>
+      <GlobalStyle/>
+      <h1>Opa</h1>
+    </Container>
+  )
+}
