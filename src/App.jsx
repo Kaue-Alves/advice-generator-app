ddadb2364b42970ef87e353b5c5679d20a06e6e9
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-12-24 03:00:37 -0300

    adiciona react-loader-spinner e implementa carregamento no botão de gerar conselho

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,37 @@ import * as S from "./App.styles.js";
 import IconGerar from "./components/IconGerar.jsx";
 import Line from "./components/IconPause.jsx";
 import { executar } from "./hooks/pegarTexto.js";
-
+import { TailSpin } from "react-loader-spinner";
 
 export default function App() {
+    const [id, setId] = useState(null);
+    const [conselho, setConselho] = useState(
+        "Clique no botão para gerar um conselho."
+    );
+    const [carregando, setCarregando] = useState(false);
 
-    const [id, setId] = useState(null)
-    const [conselho, setConselho] = useState("Clique no botão para gerar um conselho.")
-    
     async function pegarConselho() {
+        setCarregando(true);
         const receba = await executar();
-        setId(receba.id)
-        setConselho(`"${receba.advice}"`)
+        setId(receba.id);
+        setConselho(`"${receba.advice}"`);
+        setCarregando(false);
     }
-    
-    
+
     return (
         <S.TagContainer>
             <S.ContainerCard>
                 <S.Card>
                     <S.AdviceP>ADVICE #{id}</S.AdviceP>
-                    <S.MensagemP>
-                        {conselho}
-                    </S.MensagemP>
+                    <S.MensagemP>{conselho}</S.MensagemP>
                     <Line />
                 </S.Card>
                 <S.BotaoGerar onClick={pegarConselho}>
-                    <IconGerar />
+                    {carregando ? (
+                        <TailSpin height="24" width="24" color="#202733" />
+                    ) : (
+                        <IconGerar />
+                    )}
                 </S.BotaoGerar>
             </S.ContainerCard>
         </S.TagContainer>
